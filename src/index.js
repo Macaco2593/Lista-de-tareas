030342dd57a50e1b9d4b88e9af01f2ee8fa64161
Santiago <santiagopjq13@gmail.com>
2022-04-26 21:53:12 -0400

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import { saludar } from "./js/componentes"
 import "./styles.css"
 
-const nombre = "Fernando"
+import { Todo, TodoList } from "./classes"
+import { crearTodoHtml } from "./js/componentes";
 
-saludar(nombre)
+
+export const todoList = new TodoList();
+
+// const tarea = new Todo("Aprender JavaScript!!");
+// todoList.nuevoTodo ( tarea );
+
+
+// console.log( todoList)
+
+// crearTodoHtml( tarea );
+
+// localStorage.setItem("my-key", "ABC1233");
+// sessionStorage.setItem("my-key", "ABC1234")
+
+// setTimeout( () =>{
+
+//     localStorage.removeItem("my-key")
+
+// }, 1500 )
+
+// todoList.todos.forEach( todo => crearTodoHtml( todo )) Lo mismo que:
+todoList.todos.forEach( crearTodoHtml )
+
+console.log("todos", todoList.todos )
